aac540258931df90d296cac67ff03a57ffd74c52
Ajaypal Cheema <ajaypalster@gmail.com>
2017-04-20 20:07:33 -0400

    build gh-pages

--- src/index.js
+++
@@ -1,8 +0,0 @@
-/*
-* the index.js entry point
-*/
-import React from 'react';
-import { render } from 'react-dom';
-import Apod from './Apod.js';
-
-render(<Apod />, document.querySelector('#app'));
